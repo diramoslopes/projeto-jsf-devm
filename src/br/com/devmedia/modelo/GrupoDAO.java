7738b88d2d1d34ac0515432bd3c441da6693c0ac
Diogo <dramoslopesilveira@gmail.com>
2019-11-21 09:39:09 -0300

    CRUD com JPA e JSF aula 35 realizada com exito

---
+++ src/br/com/devmedia/modelo/GrupoDAO.java
@@ -0,0 +1,77 @@
+package br.com.devmedia.modelo;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.devmedia.beans.Grupo;
+import br.com.devmedia.jpa.EntityManagerUtil;
+import br.com.devmedia.util.UtilErros;
+import br.com.devmedia.util.UtilMessagens;
+
+public class GrupoDAO {
+	
+	private EntityManager em;
+	
+	public GrupoDAO() {
+		em = EntityManagerUtil.getEntityManager();
+	}
+	
+	public List<Grupo> listarTodos(){
+		return em.createQuery("from Grupo order by nome").getResultList();
+	}
+	
+	public boolean gravar(Grupo obj) {
+		try {
+			em.getTransaction().begin();
+			if (obj.getId() == null) {
+				em.persist(obj);
+			} else {
+				em.merge(obj);
+			}
+			em.getTransaction().commit();
+			UtilMessagens.mensagemInformacao("Objeto persistido com sucesso");
+			return true;
+		} catch (Exception e) {
+			if(em.getTransaction().isActive() == false) {
+				em.getTransaction().begin();
+			}
+			
+			em.getTransaction().rollback();
+			UtilMessagens.mensagemErro("Erro ao persistir objeto: " + UtilErros.getMensagemErro(e));
+			return false;
+		}
+	}
+	
+	public boolean excluir(Grupo obj) {
+		try {
+			em.getTransaction().begin();
+			em.remove(obj);
+			em.getTransaction().commit();
+			UtilMessagens.mensagemInformacao("Objeto removido com sucesso");
+			return true;
+		} catch (Exception e) {
+			if(em.getTransaction().isActive() == false) {
+				em.getTransaction().begin();
+			}
+			
+			em.getTransaction().rollback();
+			UtilMessagens.mensagemErro("Erro ao remover objeto: " + UtilErros.getMensagemErro(e));
+			return false;
+		}
+	}
+	
+	public Grupo localizar(Integer id) {
+		return em.find(Grupo.class, id);
+	}
+	
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+	
+	
+}

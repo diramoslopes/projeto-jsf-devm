4817b9f5c8d7d81598932c5f6e68fdd8edf1979b
Diogo <dramoslopesilveira@gmail.com>
2019-12-03 17:10:55 -0300

    aula 44

--- src/br/com/devmedia/controle/ControleGrupo.java
+++ src/br/com/devmedia/controle/ControleGrupo.java
@@ -3,10 +3,12 @@ package br.com.devmedia.controle;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import br.com.devmedia.beans.Grupo;
 import br.com.devmedia.modelo.GrupoDAO;
+import br.com.devmedia.util.UtilMessagens;
 
 @ManagedBean(name = "controleGrupo")
 @SessionScoped
@@ -14,6 +16,9 @@ public class ControleGrupo implements Serializable{
 	
 	private GrupoDAO dao;
 	private Grupo objeto;
+	
+	@ManagedProperty(value = "#{controleLogin}")
+	private ControleLogin controleLogin;
 	
 	public ControleGrupo() {
 		dao = new GrupoDAO();
@@ -46,7 +51,14 @@ public class ControleGrupo implements Serializable{
 	}
 	
 	public String excluir(Grupo obj) {
-		dao.excluir(obj);
+		
+		if(controleLogin.getUsuarioLogado().
+				getGrupo().getNome().equals("Administradores")) {
+			dao.excluir(obj);
+		} else {
+			UtilMessagens.mensagemErro("Usuario nao tem autorizacao para exclusão!");
+		}
+		
 		return "listar";
 	}
 	
@@ -64,5 +76,13 @@ public class ControleGrupo implements Serializable{
 	
 	public void setObjeto(Grupo obj) {
 		this.objeto = obj;
+	}
+
+	public ControleLogin getControleLogin() {
+		return controleLogin;
+	}
+
+	public void setControleLogin(ControleLogin controleLogin) {
+		this.controleLogin = controleLogin;
 	}
 }
